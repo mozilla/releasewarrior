d9d0c7eaf8f97109cae827eb85aa40c0650733b2
Jordan Lund <jlund@mozilla.com>
2016-05-16 14:41:57 -0700

    finishes pre_check_run

--- releasewarrior/commands.py
+++ releasewarrior/commands.py
@@ -14,19 +14,25 @@ class Command(metaclass=abc.ABCMeta):
         self.repo = Repo(REPO_PATH)
 
     def pre_run_check(self):
-        """ensures repo is clean and nothing is incoming"""
+        """ensures repo is clean and local repo is in sync with remote origin"""
+
         logger.info("ensuring releasewarrior repo is clean")
         if self.repo.is_dirty():
             logger.error("releasewarrior repo is dirty. aborting run to be safe.")
             sys.exit(1)
+        logger.info("repo is clean!")
 
+        # TODO - we should allow csets to exist locally that are not on remote.
+        logger.info("ensuring releasewarrior repo is up to date and in sync with origin")
         origin = self.repo.remotes.origin
+        logger.info("fetching csets from origin")
         origin.fetch()
         if self.repo.head.commit.diff(origin):
-            logger.error("diff found against origin. ensure `git diff origin/master` yields nothing")
-
-        logger.info("made it here")
-
+            logger.error("diff found against origin. ensure `git diff origin/master` yields "
+                         "nothing. if commits exist locally but are unknown to origin, "
+                         "please push them before trying again. aborting run to be safe.")
+            sys.exit(1)
+        logger.info("repo is up to date and in sync")
 
     @abc.abstractmethod
     def generate_data(self, data, data_path):
@@ -39,6 +45,8 @@ class Command(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def run(self, args):
         """run sub command"""
+        # TODO - uncomment
+        # self.pre_run_check()
 
     def release_exists(self, data_path):
         pass
@@ -53,8 +61,7 @@ class CreateRelease(Command):
         pass
 
     def run(self, args):
-        logger.debug('create run call')
-        self.pre_run_check()
+        super().run(args)
 
 
 class UpdateRelease(Command):
