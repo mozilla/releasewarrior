c71bc106ef2b8ce506d143f93754a09794d3250a
Justin Wood <Callek@gmail.com>
2016-06-27 16:51:26 -0400

    Fix bustage where issues was accidentally required

--- releasewarrior/commands.py
+++ releasewarrior/commands.py
@@ -61,7 +61,8 @@ class Command(metaclass=abc.ABCMeta):
         """generates wiki file based on data file and wiki template"""
         env = Environment(loader=FileSystemLoader(TEMPLATES_PATH),
                           undefined=StrictUndefined, trim_blocks=True)
-        data['issues'] = convert_bugs_to_links(data['issues'])
+        if 'issues' in data:
+            data['issues'] = convert_bugs_to_links(data['issues'])
         template = env.get_template(wiki_template)
         return template.render(**data)
 
